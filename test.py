c35b2be7ca63ea77e7a46e018bd639bb5c9e6ee6
Jan Böcker <jan.boecker@jboecker.de>
2015-02-05 20:59:12 +0100

    add ProtocolParser, StringBuffer, IntegerBuffer and example

---
+++ test.py
@@ -0,0 +1,25 @@
+#!python3
+
+import socket
+from dcsbios import ProtocolParser, StringBuffer, IntegerBuffer
+
+parser = ProtocolParser()
+
+# address = 0x1000, length = 19
+cmspLine1 = StringBuffer(parser, 0x1000, 19, lambda s: print("CMSP LINE 1:", s))
+
+# address = 0x1014, length = 19
+cmspLine2 = StringBuffer(parser, 0x1014, 19, lambda s: print("CMSP LINE 2:", s))
+
+# address = 0x10e4, mask = 0x3800, shift_by = 11
+cmspMode = IntegerBuffer(parser, 0x10e4, 0x3800, 11, lambda i: print("CMSP MODE: ", i))
+
+# uncomment to print all writes
+# parser.write_callbacks.add(lambda address, data: print(address, data))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 7778))
+
+while 1:
+	c = s.recv(1)
+	parser.processByte(c)
